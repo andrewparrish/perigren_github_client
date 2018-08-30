b97219d4c9b712470d66c566b0e158e2dca2949c
Andrew Parrish <m.andrewparrish@gmail.com>
2018-08-30 18:14:04 -0400

    Downing expiration time

--- lib/perigren_github_client/client.rb
+++ lib/perigren_github_client/client.rb
@@ -39,7 +39,7 @@ module PerigrenGithubClient
         # issued at time
         iat: Time.now.to_i,
         # JWT expiration time (10 minute maximum)
-        exp: Time.now.to_i + (10 * 60),
+        exp: Time.now.to_i + (9 * 60),
         # GitHub App's identifier
         iss: @app_id
       }
